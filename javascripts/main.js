aaaa756ae230c9056183247912ead84650d310a5
Nipol <yoonseulcc@gmail.com>
2015-02-05 08:51:59 +0900

    add wedding day count

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+$('#clocktime').countdown('2015/4/25 12:00:00')
+  .on('update.countdown', function(event) {
+    var format = '%H:%M:%S';
+    if(event.offset.days > 0) {
+      format = '%-d 일 ' + format;
+    }
+    if(event.offset.weeks > 0) {
+      format = '%-w 주 ' + format;
+    }
+    $(this).html(event.strftime(format));
+  })
+  .on('finish.countdown', function(event) {
+    $(this).html('This offer has expired!').parent().addClass('disabled');
+  });
